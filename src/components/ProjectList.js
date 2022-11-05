2e18b7a2d173cf9779c1d0d2a7002c02aadf062f
Lindsay Taylor <llindsayttaylor@gmail.com>
2022-11-05 12:06:29 -0400

    commit tree d37daaec24e87d6a2fb813e51bec4cd0f634f589

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,18 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+  
+
+  const projectComponents = projects.map((project) => {
+    return <ProjectItem key={project.id} {...project} />;
+  })
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {projectComponents}
+      </div>
     </div>
   );
 }
